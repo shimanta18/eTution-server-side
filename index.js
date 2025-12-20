539e290d63a16c18a09ac9ade91ff52b812f920e
Shimanta Chowdhury <scshomu@gmail.com>
2025-12-20 22:38:44 +0600

    change origin again 2

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 5000;
 
 app.use(cors({
   origin: [
-    "https://incandescent-swan-395936.netlify.app",
+    "https://bejewelled-begonia-bc4834.netlify.app",
     "http://localhost:5173"
   ],
   credentials: true
