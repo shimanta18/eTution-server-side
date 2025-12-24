d8a96e5ac1a1d0119b1a44bca96c3a2dafa70d9c
Shimanta Chowdhury <scshomu@gmail.com>
2025-12-24 19:12:10 +0600

    new origin 3

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 5000;
 
 app.use(cors({
   origin: [
-    "https://bejewelled-begonia-bc4834.netlify.app",
+    "https://venerable-starlight-82d99d.netlify.app",
     "http://localhost:5173"
   ],
   credentials: true
