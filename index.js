32d31d268666c17110ce042176989f9ed3ac4746
Shimanta Chowdhury <scshomu@gmail.com>
2025-12-24 20:21:20 +0600

    origin 7

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 5000;
 
 app.use(cors({
   origin: [
-    "https://boisterous-nasturtium-6c63b5.netlify.app",
+    "https://symphonious-souffle-dea74b.netlify.app",
     "http://localhost:5173"
   ],
   credentials: true
