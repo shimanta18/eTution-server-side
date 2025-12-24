e41ec4d082425f319832d3a2826f986bddd67e5a
Shimanta Chowdhury <scshomu@gmail.com>
2025-12-24 20:39:50 +0600

    new

--- index.js
+++ index.js
@@ -58,7 +58,10 @@ async function run() {
         res.status(500).json({ error: 'Server error' });
       }
     })
-
+app.use((req, res, next) => {
+  console.log(`Incoming Request: ${req.method} ${req.url}`);
+  next();
+});
     //  tuition by ID
 app.get('/api/tuitions/:id', async (req, res) => {
   try {
